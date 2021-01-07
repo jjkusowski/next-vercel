0c900ecf7cc9eb613ba308fd94a3504d2d1c0efb
buck rivard <johrivar@cisco.com>
2021-01-07 10:22:58 -0800

    fix eslint grumpiness

--- components/NavSubmenus/Resources.tsx
+++ components/NavSubmenus/Resources.tsx
@@ -1,21 +1,21 @@
 import data from "./submenu.json";
 import styles from "./navbar.module.css";
-        
+
 const Resources = () => {
   return (
-    <div className="w-auto h-auto lg:fixed left-0 right-0 top-20 pl-0 pt-0 mt-6 bg-white text-left">
+    <div className="left-0 right-0 w-auto h-auto pt-0 pl-0 mt-6 text-left bg-white lg:fixed top-20">
       <div
         className={`px-8 lg:px-32 w-full ${styles["border-top"]} relative left-0 mx-auto`}
       >
-        <div className="px-0 py-8 md:py-12 list-none flex flex-wrap -mx-12 md:-mx-8">
-          <ul className="w-full flex flex-wrap list-none m-0 p-0 font-normal antialiased">
+        <div className="flex flex-wrap px-0 py-8 -mx-12 list-none md:py-12 md:-mx-8">
+          <ul className="flex flex-wrap w-full p-0 m-0 antialiased font-normal list-none">
             {data.resources.map((s) => (
               <li
-                className="text-sm font-normal px-4 mb-0 w-full md:w-1/2 lg:w-1/3 md:h-40 py-2 md:py-0"
+                className="w-full px-4 py-2 mb-0 text-sm font-normal md:w-1/2 lg:w-1/3 md:h-40 md:py-0"
                 key={s.id}
               >
                 <a
-                  className=" text-base lg:text-lg text-black font-light leading-loose"
+                  className="text-base font-light leading-loose text-black  lg:text-lg"
                   target="_blank"
                   rel="noreferrer"
                   href={s.href}
