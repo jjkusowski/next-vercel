02ef9464b2fb34be37b352a8e51f17604ba6239c
buck rivard <johrivar@cisco.com>
2021-01-11 14:57:22 -0800

    refactor to use next/image component

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 const assetPrefix = "/2/wdc2/";
 
 module.exports = {
-  assetPrefix: isProd ? assetPrefix : "",
+  // assetPrefix: isProd ? assetPrefix : "",
   // eslint-disable-next-line global-require
   i18n: require("./config/locales.js"),
   images: {
