ceec7dad08c2b71b68d981e84ea461ad886f24b0
jkusowsk <jkusowsk@cisco.com>
2021-01-11 13:36:02 -0800

    feat(seo): add meta data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,6 +26,17 @@ class MyDocument extends Document {
           <meta property="og:type" content="website" />
           <meta name="twitter:card" content="summary" key="twcard" />
           <meta name="twitter:creator" content="@Webex" key="twhandle" />
+          <link
+            rel="chrome-webstore-item"
+            href="https://chrome.google.com/webstore/detail/cisco-webex-scheduler/ankblejcdieljecjfagjeoelaedaoaka"
+          />
+          <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
+          <meta name="keywords" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="X-UA-Compatible" content="IE=edge,chrome=1" />
+          <meta name="language" content="en" />
+          <meta name="country" content="US" />
+
           <Analytics />
         </Head>
         <body>
