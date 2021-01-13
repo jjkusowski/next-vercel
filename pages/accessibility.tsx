3c9794ef12d189624f0c949683719b69a8011ef3
buck rivard <johrivar@cisco.com>
2021-01-13 09:11:52 -0800

    perf: blur hero image

--- pages/accessibility.tsx
+++ pages/accessibility.tsx
@@ -5,6 +5,8 @@ import { Elements } from "prismic-reactjs";
 import Head from "next/head";
 import { defineMessages, useIntl } from "react-intl";
 import Image from "next/image";
+import { getImage } from "@plaiceholder/next";
+import { getBase64 } from "@plaiceholder/base64";
 import PageContext from "../state/PageContext";
 import Layout from "../components/Layout";
 import Text from "../components/Text";
@@ -24,6 +26,13 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
 
   const data = await restQuery;
 
+  const imgSrc = "/accessibility-hero.webp";
+  const img = await getImage(imgSrc);
+  const imgBase64 = await getBase64(img);
+
+  const hero = data.body.find((component) => component.slice_type === "hero");
+  hero.imgBase64 = imgBase64;
+
   return {
     props: {
       data,
@@ -32,7 +41,7 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
 };
 
 const Hero = (props) => {
-  const { primary } = props;
+  const { primary, imgBase64 } = props;
   const { image, text } = primary;
   const { dimensions, alt, url } = image;
   const { width, height } = dimensions;
@@ -56,7 +65,29 @@ const Hero = (props) => {
 
   return (
     <section className="pt-0 mt-0 md:container lg:relative lg:mb-10">
-      <Image src={url} alt={alt} height={height} width={width} priority />
+      <div className="relative">
+        <img
+          aria-hidden="true"
+          alt=""
+          src={imgBase64}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            width,
+            height,
+            /* Adjust the content to fit */
+            objectFit: "cover",
+            objectPosition: "center",
+            /* Blur the image and scale to avoid transparent corners */
+            filter: "blur(1.5rem)",
+            transform: "scale(0.9)",
+          }}
+        />
+        <Image src={url} alt={alt} height={height} width={width} priority />
+      </div>
       <div className="relative grid gap-6 p-10 bg-white md:container md:mr-0 md:-mt-12 lg:absolute lg:bottom-0 lg:right-0 lg:px-16 lg:w-1/2 lg:-m-8 lg:h-full lg:mr-8">
         <div>
           <Text htmlSerializer={htmlSerializer}>{h1s}</Text>
